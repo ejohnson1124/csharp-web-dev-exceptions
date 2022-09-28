5fc5a57fc8e9967f2336f2c5d84d780f0f3d5cb1
Erica <johnson.ericastarr@gmail.com>
2022-09-28 17:44:42 -0500

    commit

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,22 +7,50 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
-        static double Divide(double x, double y)
-        {
-            // Write your code here!
-        }
-
+            static double Divide(double x, double y)
+   {
+      if (y == 0.0)
+      {
+         throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!");
+      }
+      else
+      {
+         return x / y;
+      }
+   }
+        
         static int CheckFileExtension(string fileName)
-        {
-            // Write your code here!
-        }
-        */
-
+   {
+      if (fileName == null || fileName == "")
+      {
+         throw new ArgumentNullException("fileName","Student did not submit any work!");
+      }
+      else
+      {
+         if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+         {
+            return 1;
+         }
+         else
+         {
+            return 0;
+         }
+      }
+   }
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+             double num1 = 8;
+            double num2 = 0;
 
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
@@ -30,7 +58,17 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+             foreach (string s in students.Values)
+            {
+                try
+                {
+                    Console.WriteLine(CheckFileExtension(s));
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
